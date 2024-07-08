1cdf6e4261729ea319cbe1406ffcfabe0bcf27e2
sagar wankhede <swankhede639@gmail.com>
2024-07-08 15:31:11 +0530

    calling api

---
+++ src/apiCall/api.js
@@ -0,0 +1,11 @@
+const apiKey = '22c349318c0d52e995f9ea6230dc9805';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
