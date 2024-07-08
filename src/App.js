1a8676dc06d5fe82a6123bf05605412b9c71100e
sagar wankhede <swankhede639@gmail.com>
2024-07-08 14:48:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Weather App</h1>
+      <div className="input-wrapper">
+        <input type="text" placeholder="Enter City Name" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
